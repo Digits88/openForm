d8e2acf8b4e4c3d05df7e9d33e6ba1a447309906
John Ombagi T <ombagi@promaxted.com>
2014-03-15 19:30:03 +0300

    Inital commit

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+
+$email_to = "your@email.com"; // your email address
+$email_subject = "form submission"; // email subject line
+$thankyou = "thankyou.html"; // load another thank-you page
+
+//updating the question on the form = updating question answer below
+
+$antispam_answer = "35";
+
+?>
